c687f2b22c07b0842ee5143714ba66a40a57aa76
Isaac Santana Souza <isaacsantana.162@gmail.com>
2024-07-28 14:44:20 -0300

    Setup .env properly

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,9 @@
 import os
 from pynubank import Nubank
 from dotenv import load_dotenv
+from pathlib import Path
 
-
-load_dotenv()
+load_dotenv(Path(dotenv_path="../config/credentials.env"))
 
 user = {
     "cpf" : os.getenv("CPF"),
@@ -13,5 +13,6 @@ user = {
 
 nu = Nubank()
 
-nu.authenticate_with_cert(user["cpf"], user["passwd"], user["cert_path"])
-print(nu.get_account_balance())
+#nu.authenticate_with_cert(user["cpf"], user["passwd"], user["cert_path"])
+#print(nu.get_account_balance())
+print(user["cpf"])
