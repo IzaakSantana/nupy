2bed9e05ff6d62b8ca286cdb1d30226fc5eec3b3
Isaac Santana Souza <isaacsantana.162@gmail.com>
2024-07-29 02:15:43 -0300

    Changed how the credentials are accessed by using a class

--- scripts/main.py
+++ scripts/main.py
@@ -1,20 +1,38 @@
 import os
 from pynubank import Nubank
 from dotenv import load_dotenv
-from pathlib import Path
 
 load_dotenv()
 
-os.chdir("M:\\Documentos\\Nubank")
+class UserData:
+    def __init__(self, cpf, passwd, cert_path):
+        self._cpf = cpf
+        self._passwd = passwd
+        self._cert_path = cert_path
 
-user = {
-    "cpf" : os.getenv("CPF"),
-    "passwd" : os.getenv("PASSWD"),
-    "cert_path" : os.getenv("CERT_PATH")
-}
+
+    @property
+    def cpf(self):
+        return self._cpf
+    
+
+    @property
+    def passwd(self):
+        return self._passwd
+    
+
+    @property
+    def cert_path(self):
+        return self._cert_path
+    
+
+user = UserData(
+    cpf=os.getenv("CPF"),
+    passwd=os.getenv("PASSWD"),
+    cert_path=os.getenv("CERT_PATH")
+)
 
 nu = Nubank()
 
-nu.authenticate_with_cert(user["cpf"], user["passwd"], "./cert.p12")
-print(nu.get_account_balance())
-#print(user["cert_path"])
+nu.authenticate_with_cert(user.cpf, user.passwd, user.cert_path)
+print(nu.get_bills())
