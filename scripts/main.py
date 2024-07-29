1359bb006423d100c9821e30927ed4dbde081f6a
Isaac Santana Souza <isaacsantana.162@gmail.com>
2024-07-29 03:48:26 -0300

    Change project to work with Pluggy.ai and setup first test

--- scripts/main.py
+++ scripts/main.py
@@ -1,38 +1,40 @@
-import os
-from pynubank import Nubank
+import requests
+from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class UserData:
-    def __init__(self, cpf, passwd, cert_path):
-        self._cpf = cpf
-        self._passwd = passwd
-        self._cert_path = cert_path
+class Pluggy:
+    def __init__(self, client_id, client_secret):
+        self._url = "https://api.pluggy.ai"
+        self._headers = {"accept": "application/jason", "content-type": "application/jason"}
+        self._api_key = None
+        self._client_id = client_id
+        self._client_secret = client_secret
 
 
     @property
-    def cpf(self):
-        return self._cpf
-    
+    def api_key(self):
+        return self._api_key
+
 
     @property
-    def passwd(self):
-        return self._passwd
-    
+    def url(self):
+        return self._url
 
-    @property
-    def cert_path(self):
-        return self._cert_path
-    
 
-user = UserData(
-    cpf=os.getenv("CPF"),
-    passwd=os.getenv("PASSWD"),
-    cert_path=os.getenv("CERT_PATH")
-)
+    def create_api_key(self):
+        endpoint = "/auth"
 
-nu = Nubank()
+        payload = {
+            "clientId": self._client_id,
+            "clientSecret": self._client_secret
+        }
 
-nu.authenticate_with_cert(user.cpf, user.passwd, user.cert_path)
-print(nu.get_bills())
+        self._api_key = requests.post(self._url + endpoint, headers=self._headers, json=payload).text
+
+         
+
+pluggy = Pluggy(getenv("CLIENT_ID"), getenv("CLIENT_SECRET"))
+pluggy.create_api_key()
+print(pluggy.api_key)
